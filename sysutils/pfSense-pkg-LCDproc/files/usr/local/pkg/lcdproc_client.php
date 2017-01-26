876f10e07d70d94b4e88e9603703802f26d21082
Treer <treer.git@the-bordello.com>
2017-01-27 01:07:43 +1100

    Fix another bug with displaying load averages Even with the fix in ff79e7e841c1f89e84d1eeb2b7306636bc3b90fe, get_loadavg_stats() still fails on input such as: 12:19AM  up  2:38, 1 user, load averages: 0.25, 0.19, 0.11
    So instead of trying to predict how many words preceed the averages, I take the last 3, this works on varied uptime outputs, e.g:
    12:19AM  up  2:38, 1 user, load averages: 0.25, 0.19, 0.11
     1:00AM  up 7 mins, 2 users, load averages: 0.23, 0.26, 0.17
     8:24PM  up 1 day,  1:02, 1 user, load averages: 0.54, 0.40, 0.25

--- sysutils/pfSense-pkg-LCDproc/files/usr/local/pkg/lcdproc_client.php
+++ sysutils/pfSense-pkg-LCDproc/files/usr/local/pkg/lcdproc_client.php
@@ -103,14 +103,10 @@ function get_uptime_stats() {
 
 function get_loadavg_stats() {
 	exec("/usr/bin/uptime", $output, $ret);
-	if (stristr($output[0], "day")) {
-		$temp = preg_split("/ /", $output[0], -1, PREG_SPLIT_NO_EMPTY);
-		$status = "$temp[9] $temp[10] $temp[11]";
-	} else {
-		$temp = preg_split("/ /", $output[0], -1, PREG_SPLIT_NO_EMPTY);
-		$status = "$temp[8] $temp[9] $temp[10]";
-	}
-	return($status);
+
+	$temp = preg_split("/ /", $output[0], -1, PREG_SPLIT_NO_EMPTY);
+	$count = count($temp);
+	return ($count >= 3) ? "{$temp[$count - 3]} {$temp[$count - 2]} {$temp[$count - 1]}" : "Not available";
 }
 
 function get_mbuf_stats() {
